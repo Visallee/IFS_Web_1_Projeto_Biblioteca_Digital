9b3d93d5e0f863d161bd0c94a45c313726812133
Victor Silva do Valle <victor.silvadovalle@gmail.com>
2026-05-29 16:04:18 -0300

    Criando primeiras tabelas

--- apps/biblioteca/models.py
+++ apps/biblioteca/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Livro(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='livros')
+
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=20, unique=True)
+    ano_publicacao = models.IntegerField()
+    quantidade_disponivel = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.titulo} - {self.autor}"
